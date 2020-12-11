4aa9ce54c36029959499038012300dee1774be18
swapna1704 <73925905+swapna1704@users.noreply.github.com>
2020-12-11 10:11:31 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Size of char:" << sizeof(char) << "byte" << endl;
+    cout << "Size of intr:" << sizeof(int) << "byte" << endl;
+    cout << "Size of float:" << sizeof(float) << "byte" << endl;
+    cout << "Size of double:" << sizeof(double) << "byte" << endl;
+    system("pause");
+}
